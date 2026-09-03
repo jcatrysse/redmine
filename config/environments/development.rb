075c86e8a0179279b92c96be3b4e3d5fee2913ed
Jan Catrysse <jan.catrysse@geoxyz.eu>
2026-09-03 14:37:11 +0000

    Allow development requests to the geoxyz.eu subdomains.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.hosts << /.*\.geoxyz\.eu/
+
   config.after_initialize do
     Bullet.enable = true
     Bullet.rails_logger = true
