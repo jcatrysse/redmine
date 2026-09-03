95bbb9750c0306072164f0285a6bea60ca8ea5c1
Jan Catrysse <jan.catrysse@geoxyz.eu>
2026-09-03 14:40:22 +0000

    Store sessions in the database instead of in the session cookie.

---
+++ db/migrate/20240929111106_add_sessions_table.rb
@@ -0,0 +1,14 @@
+class AddSessionsTable < ActiveRecord::Migration[8.1]
+  def change
+    return if table_exists?(:sessions)
+
+    create_table :sessions do |t|
+      t.string :session_id, null: false
+      t.text :data
+      t.timestamps
+    end
+
+    add_index :sessions, :session_id, unique: true
+    add_index :sessions, :updated_at
+  end
+end
